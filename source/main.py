1a0db33612ace47dca72d00b11a560df16b2a4d1
dokibal <doktor.balazs1@gmail.com>
2023-01-02 11:47:05 +0100

    Initialize UI
    -Add main qt .ui file
    -Add a custom widget mplcanvas
    -Add entry py file main.py

--- source/main.py
+++ source/main.py
@@ -1,20 +1,35 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
-
+from PyQt5 import QtWidgets
+from ui.mainwindow import Ui_CryptoWindow
+from database.dbhandler import DbHandler
 import sys
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(ApplicationWindow, self).__init__()
 
-        self.setWindowTitle("Hello World")
+        self.db_handler = DbHandler()
+        self.db_handler.connect()
 
-        button = QPushButton("My simple app.")
-        button.pressed.connect(self.close)
+        self.ui = Ui_CryptoWindow()
+        self.ui.setupUi(self)
+        
+        sc = self.ui.graph
+        prices = self.get_points()
 
-        self.setCentralWidget(button)
+        print(prices)
+        sc.axes.plot(prices[0],prices[1])
+        self.setCentralWidget(sc)
         self.show()
 
+    def get_points(self):
+        prices = self.db_handler.get_all_prices()
+        return prices
 
-app = QApplication(sys.argv)
-w = MainWindow()
-app.exec()
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
